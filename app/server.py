82e5a54aa93705efc9431f4a740abdd0f798237c
KuperOK <kupershtein.lm@gmail.com>
2024-11-05 23:35:17 +0200

    Initial commit

---
+++ app/server.py
@@ -0,0 +1,47 @@
+from decouple import config
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+from langchain_openai import ChatOpenAI
+# from langchain_core.prompts.prompt
+from langchain_core.prompts.prompt import PromptTemplate
+from langchain.prompts import ChatPromptTemplate
+from langserve import add_routes
+# from pydantic import BaseModel
+
+app = FastAPI()
+
+# define model
+model = ChatOpenAI(
+    openai_api_key=config("OPENAI_API_KEY"),
+    model='gpt-4o-mini',
+    temperature=0.4)
+
+# Define a prompt
+template = """
+What is the meaning of {input}?
+Answer as if you are a {profession}.
+Give short answer in {language}.
+Firstly dublicate question and than answer as follows:
+
+Q: only human question
+A: AI assistant answer
+"""
+
+prompt = PromptTemplate(
+    template=template,
+    input_variables=['input', 'profession', 'language']
+)
+
+chain = prompt | model
+
+@app.get("/")
+async def redirect_root_to_docs():
+    return RedirectResponse("/docs")
+
+
+# Edit this to add the chain you want to add
+add_routes(app, chain, path='/ask_openai')
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
