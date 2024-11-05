94e2f1918c566c43e69f3f8f4ea646bd309e9d9b
KuperOK <kupershtein.lm@gmail.com>
2024-11-06 01:24:54 +0200

    v03 getenv

--- app/server.py
+++ app/server.py
@@ -1,18 +1,22 @@
-from decouple import config
+# from decouple import config
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from langchain_openai import ChatOpenAI
 # from langchain_core.prompts.prompt
 from langchain_core.prompts.prompt import PromptTemplate
-from langchain.prompts import ChatPromptTemplate
+# from langchain.prompts import ChatPromptTemplate
 from langserve import add_routes
 # from pydantic import BaseModel
+import os
 
 app = FastAPI()
 
 # define model
+
+openai_api_key = os.getenv("OPENAI_API_KEY")
+
 model = ChatOpenAI(
-    openai_api_key=config("OPENAI_API_KEY"),
+    openai_api_key=openai_api_key #config("OPENAI_API_KEY"),
     model='gpt-4o-mini',
     temperature=0.4)
 
